213187869021b59bfb9ed8e5a7b645de5d788e6c
Hyeojin Jhon <hjhjhahaha56@naver.com>
2020-02-07 21:26:18 +0900

    7576. Tomato success!

--- group_study/7576번_토마토.cpp
+++ group_study/7576번_토마토.cpp
@@ -3,73 +3,83 @@
 #include <queue>
 
 using namespace std;
-int len, height;
+
+int m, n;// m이 가로로 몇개 있는지, n이 세로로 몇개 있는 지 
 int **data;
 struct point {
     int x;
     int y;
 };
 queue<point> que;
-int cnt=1;
 int nextt[4][2]={{0,1},{1,0},{0,-1},{-1,0}};
-int max_res=0;
+int max_res=1;
 
-int dfs(){
-    while(que.size()!=0){
-        //cout<<"dfs front:"<<" / "<<que.front().x<<" / "<<que.front().y<<" / "<<"/data:"<<data[que.front().x][que.front().y]<<" / " <<cnt<<" / "<<que.size()<<endl;
-        point tmp= que.front();
+void dfs(){
+    //큐가 빌 때 까지 반복 
+    while(!que.empty()){
+        //큐의 가장 첫번째 요소 꺼내옴 
+        point tmp=que.front();
         que.pop();
+        //동서남북 검사 
         for (int j=0;j<4;j++){
             int nextx=tmp.x+nextt[j][0];
             int nexty=tmp.y+nextt[j][1];
-            //cout<<"dfs"<<" / "<<nextx<<" / "<<nexty<<" / ";
-            if(0<=nextx and nextx<height and 0<=nexty and nexty<len and data[nextx][nexty]==0){
-                //cout<<"----------->queue push";
+            // 좌표가 배열 보다 큰지 검사 
+            if (nextx<0 || nexty<0 || nextx>=n || nexty>=m)continue;
+            //아직 안 들렀는지 검사 
+            if (data[nextx][nexty]==0){
+                //이전 좌표의 +1 한 값 데이터로 넣어줌 
                 data[nextx][nexty]=data[tmp.x][tmp.y]+1;
+                //최대 값 구하기 
                 max_res=max(max_res,data[nextx][nexty]);
+                //큐에 삽입 
                 que.push({nextx,nexty});
             }
-           // cout<<endl;
+            
         }
     }
 }
 
 
 int main(){
-    cin>>len>>height;
+    //세로, 가로 입력 
+    cin>>m>>n;
 
-    data=new int*[len];
-    for (int i=0;i<height;i++){
-        data[i]=new int[len];
-        memset(data[i],0,sizeof(int)*len);
+    //동적 배열 초기화 
+    data=new int*[n];
+    for(int i=0;i<n;i++){
+        data[i]=new int[m];
+        memset(data[i],0,sizeof(int)*m);
     }
 
-    for (int i=0;i<height;i++){
-        for (int j=0;j<len;j++){
+    //배열 입력 받음 
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
             cin>>data[i][j];
         }
     }
 
-    for (int i=0;i<height;i++){
-        for (int j=0;j<len;j++){
+    //토마토가 있는 위치 찾기 -> queue에 집어 넣음 
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
             if (data[i][j]==1){
                 que.push({i,j});
             }
         }
     }
+    //토마토 전염 시작 
     dfs();
-    int max_res=0;
-        for (int i=0;i<height;i++){
-            for (int j=0;j<len;j++){
-                if (data[i][j]==0){
-                    cout<<"-1";return 0;
-                }
-                // else{
-                //     max_res=max(max_res,data[i][j]);
-                // }
+    
+    //안익은 토마토가 있는지 확인
+    for (int i=0;i<n;i++){
+        for (int j=0;j<m;j++){
+            if (data[i][j]==0){
+                cout<<"-1";
+                return 0;
             }
         }
-        cout<<max_res-1;
+    }
+    cout<<max_res-1;
 
     return 0;
 
